cc54ec5775b34236a2f3493d91f2342ec0959803
DearDiary0802 <79373277+DearDiary0802@users.noreply.github.com>
2021-03-29 18:07:18 +0300

    Initial commit

---
+++ LR2_CSaN/ICMPPacket.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace LR2_CSaN
+{
+    class ICMPPacket
+    {
+        const int ICMP_HEADER_SIZE = 4;
+        const int IP_HEADER_SIZE = 20;
+        const int MESSAGE_SIZE = 128;
+        const int TYPE_ECHO_REQUEST = 8;
+
+        private byte code;
+        private UInt16 checksum;
+        private UInt16 seq;
+        private UInt16 id;
+        private int messageSize;
+        private byte[] message = new byte[MESSAGE_SIZE];
+        public byte Type { get; set; }
+        public int PacketSize { get; set; }
+
+        public ICMPPacket(byte[] data)
+        {
+            Type = TYPE_ECHO_REQUEST;
+            code = 0;
+            Random random = new Random();
+            seq = (UInt16)random.Next(0, UInt16.MaxValue);
+            id = (UInt16)random.Next(0, UInt16.MaxValue);
+            Buffer.BlockCopy(data, 0, message, ICMP_HEADER_SIZE, data.Length);
+            messageSize = data.Length + 4;
+            PacketSize = messageSize + ICMP_HEADER_SIZE;
+            Buffer.BlockCopy(BitConverter.GetBytes(id), 0, message, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(seq), 0, message, 2, 2);
+            checksum = getChecksum();
+        }
+
+        public ICMPPacket(byte[] data, int size)
+        {
+            Type = data[IP_HEADER_SIZE];
+            code = data[IP_HEADER_SIZE + 1];
+            checksum = BitConverter.ToUInt16(data, IP_HEADER_SIZE + 2);
+            PacketSize = size - IP_HEADER_SIZE;
+            messageSize = size - IP_HEADER_SIZE - ICMP_HEADER_SIZE;
+            Buffer.BlockCopy(data, IP_HEADER_SIZE + ICMP_HEADER_SIZE, message, 0, messageSize);
+        }
+
+        public byte[] getBytes()
+        {
+            byte[] data = new byte[PacketSize + 1];
+            Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(code), 0, data, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(checksum), 0, data, 2, 2);
+            Buffer.BlockCopy(message, 0, data, ICMP_HEADER_SIZE, messageSize);
+            return data;
+        }
+
+        public void incSeq()
+        {
+            UInt16 inc = 1;
+            byte[] bytes = BitConverter.GetBytes(inc);
+            Array.Reverse(bytes, 0, bytes.Length);
+            inc = BitConverter.ToUInt16(bytes, 0);
+            seq += inc;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(seq), 0, message, 2, 2);
+            checksum = 0;
+            checksum = getChecksum();
+        }
+
+        public UInt16 getChecksum()
+        {
+            UInt32 checksum = 0;
+            byte[] data = getBytes();
+            int index = 0;
+
+            while (index < PacketSize)
+            {
+                checksum += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                index += 2;
+            }
+            checksum = (checksum >> 16) + (checksum & 0xffff);
+            checksum += (checksum >> 16);
+            return (UInt16)(~checksum);
+        }
+    }
+}
---
+++ LR2_CSaN/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LR2_CSaN
+{
+    class Program
+    {
+        const int TYPE_ECHO_RESPONSE = 0;
+        const int TYPE_TTL_EXCEEDED = 11;
+        const int MESSAGE_SIZE = 128;
+
+        static bool showName = true;
+        static byte[] message = Encoding.ASCII.GetBytes("0000000000000000000000000000000000000000000000000000000000000000");
+
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            string name;
+            if (args.Length == 1)
+            {
+                name = args[0];
+            }
+            else if (args.Length == 2 && args[0] == "-d")
+            {
+                name = args[1];
+                showName = false;
+            }
+            else
+            {
+                Console.Write("Mytracert ");
+                name = Console.ReadLine();
+            }
+
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(name);
+                IPEndPoint destIP = new IPEndPoint(ipHost.AddressList[0], 0);
+                for (int index = 1; index < ipHost.AddressList.Length; index++)
+                {
+                    if (ipHost.AddressList[index].ToString() == name)
+                    {
+                        destIP = new IPEndPoint(ipHost.AddressList[index], 0);
+                    } 
+                }
+
+                ICMPPacket packet = new ICMPPacket(message);
+
+                string ResultIP = destIP.ToString();
+                ResultIP = ResultIP.Remove(ResultIP.IndexOf(':'), ResultIP.Length - ResultIP.IndexOf(':'));
+                try
+                {
+                    if (showName)
+                    {
+                        string ResultHostName = Dns.GetHostEntry(ResultIP).HostName;
+                        if (ResultHostName.IndexOf(name) != -1)
+                            Console.WriteLine("Трассировка маршрута к {0} [{1}]", name, ResultIP);
+                        else
+                            Console.WriteLine("Трассировка маршрута к {0} [{1}]", ResultHostName, ResultIP);
+                    }
+                    else
+                        Console.WriteLine("Трассировка маршрута к {0}", ResultIP);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Трассировка маршрута к {0}", ResultIP);
+                }
+                Console.WriteLine("с максимальным числом прыжков 30: ");
+
+                Traceroute(socket, packet, destIP);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Не удается разрешить системное имя узла {0}.", name);
+            }            
+            socket.Close();
+        }
+
+        static void Traceroute(Socket socket, ICMPPacket packet, IPEndPoint destIP)
+        {
+            int responseSize;
+            DateTime timeStart, timeEnd;
+            byte[] responseMessage;
+        
+            EndPoint hopIP = destIP;
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+            for (int i = 1; i < 31; i++)
+            {
+                EndPoint BufPoint = null;
+                Console.Write("{0, 3} ", i);
+                for (int j = 0; j < 3; j++)
+                {
+                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, i);
+                    timeStart = DateTime.Now;
+                    socket.SendTo(packet.getBytes(), packet.PacketSize, SocketFlags.None, destIP);
+                    try
+                    {
+                        responseMessage = new byte[MESSAGE_SIZE];
+                        responseSize = socket.ReceiveFrom(responseMessage, ref hopIP);
+                        timeEnd = DateTime.Now;
+                        ICMPPacket response = new ICMPPacket(responseMessage, responseSize);
+                        if ((response.Type == TYPE_ECHO_RESPONSE) || (response.Type == TYPE_TTL_EXCEEDED))
+                        {
+                            BufPoint = hopIP;
+                            Console.Write("{0, 5} ms", (timeEnd.Ticks - timeStart.Ticks) / 10000);
+                            if (j == 2)
+                            {
+                                string IP = hopIP.ToString();
+                                IP = IP.Remove(IP.IndexOf(':'), IP.Length - IP.IndexOf(':'));
+                                try
+                                {
+                                    if (showName)
+                                    {
+                                        string HostName = Dns.GetHostEntry(IP).HostName;
+                                        Console.WriteLine("   {0} [{1}]", HostName, IP);
+                                    }
+                                    else
+                                        Console.WriteLine("   {0}", IP);
+                                }
+                                catch (SocketException)
+                                {
+                                    Console.WriteLine("   {0}", IP);
+                                }
+                            }
+                        }
+                        if ((response.Type == TYPE_ECHO_RESPONSE) && (j == 2))
+                        {
+                            Console.WriteLine("\nТрассировка завершена.");
+                            return;
+                        }
+                    }
+                    catch (SocketException)
+                    {
+                        Console.Write("    *   ");
+                        if (j == 2 && BufPoint == null)
+                        {
+                            Console.WriteLine("   Превышен интервал ожидания для запроса.", i);
+                        }
+                        else if (j == 2)
+                        {
+                            string IP = hopIP.ToString();
+                            IP = IP.Remove(IP.IndexOf(':'), IP.Length - IP.IndexOf(':'));
+                            try
+                            {
+                                if (showName)
+                                {
+                                    string HostName = Dns.GetHostEntry(IP).HostName;
+                                    Console.WriteLine("{0} [{1}]", IP, HostName);
+                                }
+                                else
+                                    Console.WriteLine("{0}", IP);
+                            }
+                            catch (SocketException)
+                            {
+                                Console.WriteLine("{0}", IP);
+                            }
+                        }
+                    }
+                    packet.incSeq();
+                }
+                if (i == 30)
+                {
+                    Console.WriteLine("Невозможно связаться с удаленным хостом.");
+                }
+            }
+        }
+    }
+}
